476a53648dd29f4b867bb2a21e2498330a896f66
Jânio Alberto da Nóbrega Júnior <87720450+NobreJanio@users.noreply.github.com>
2022-06-13 17:33:09 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1')
+let instrucoes = document.querySelector('#instrucoes')
+let aviso = document.querySelector('#aviso')
+//let respostaEsta = document.querySelector('#respostaEsta')
+let pontos = 0 // pontos para o placar
+let placar = 0 // placar
+
+// PERGUNTA
+let numQuestao = document.querySelector('#numQuestao')
+let pergunta   = document.querySelector('#pergunta')
