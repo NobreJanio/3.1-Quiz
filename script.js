31664cd74d44d0a795f274884795ab1ba8a49413
Jânio Alberto da Nóbrega Júnior <87720450+NobreJanio@users.noreply.github.com>
2022-06-13 23:26:23 -0300

    corrigido e finalizado

--- script.js
+++ script.js
@@ -1,192 +1,160 @@
-let titulo = document.querySelector('h1')
+/* App JS com JSON */
+let titulo = document.querySelector('#titulo')
+
 let instrucoes = document.querySelector('#instrucoes')
 let aviso = document.querySelector('#aviso')
-//let respostaEsta = document.querySelector('#respostaEsta')
-let pontos = 0 // pontos para o placar
-let placar = 0 // placar
 
-// PERGUNTA
-let numQuestao = document.querySelector('#numQuestao')
-let pergunta   = document.querySelector('#pergunta')
+// article da questao
+let questao = document.querySelector('.questao')
 
-// ALTERNATIVAS
+let nQuestao = document.querySelector('#nQuestao')
+let pergunta = document.querySelector('#pergunta')
+
 let a = document.querySelector('#a')
 let b = document.querySelector('#b')
 let c = document.querySelector('#c')
-
-// article com a class questoes
-let articleQuestoes = document.querySelector('.questoes')
-// ol li com as alternativas
-let alternativas = document.querySelector('#alternativas')
-
-const q0 = {
-    numQuestao   : 0,
-    pergunta     : "Pergunta",
-    alternativaA : "Alternativa A",
-    alternativaB : "Alternativa B",
-    alternativaC : "Alternativa C",
-    correta      : "0",
-}
-
-const q1 = {
-    numQuestao   : 1,
-    pergunta     : "Boreal é o mesmo que...",
-    alternativaA : "Sul",
-    alternativaB : "Leste",
-    alternativaC : "Norte",
-    correta      : "Norte",
-}
-
-const q2 = {
-    numQuestao   : 2,
-    pergunta     : "Austral é o mesmo que...",
-    alternativaA : "Oeste",
-    alternativaB : "Sul",
-    alternativaC : "Norte",
-    correta      : "Sul",
-}
-
-const q3 = {
-    numQuestao   : 3,
-    pergunta     : "Nascente é o mesmo que...",
-    alternativaA : "Sul",
-    alternativaB : "Leste",
-    alternativaC : "Oeste",
-    correta      : "Leste",
-}
-
-const q4 = {
-    numQuestao   : 4,
-    pergunta     : "Poente é o mesmo que...",
-    alternativaA : "Norte",
-    alternativaB : "Leste",
-    alternativaC : "Oeste",
-    correta      : "Oeste",
-}
-
-const q5 = {
-    numQuestao   : 5,
-    pergunta     : "O Brasil se localiza em qual continente?",
-    alternativaA : "África",
-    alternativaB : "Europa",
-    alternativaC : "América",
-    correta      : "América",
-}
-
-// CONSTANTE COM UM ARRAY DE OBJETOS COM TODAS AS QUESTOES
-const questoes = [q0, q1, q2, q3, q4, q5]
+let d = document.querySelector('#d')
 
 let numero = document.querySelector('#numero')
 let total  = document.querySelector('#total')
 
-numero.textContent = q1.numQuestao
+titulo.textContent = "JJ Quiz"
+let totalDeQuestoes = 0
+numero.textContent  = 1
 
-let totalDeQuestoes = (questoes.length)-1
-console.log("Total de questões " + totalDeQuestoes)
-total.textContent = totalDeQuestoes
+let pontos = 0
+let placar = document.querySelector('.placar')
 
-// MONTAR A 1a QUESTAO COMPLETA, para iniciar o Quiz
-numQuestao.textContent = q1.numQuestao
-pergunta.textContent   = q1.pergunta
-a.textContent = q1.alternativaA
-b.textContent = q1.alternativaB
-c.textContent = q1.alternativaC
+let somAcerto   = document.querySelector('#somAcerto')
+let somErro     = document.querySelector('#somErro')
+let somAplausos = document.querySelector('#somAplausos')
 
-// CONFIGURAR O VALUE INICIAL DA 1a QUESTAO COMPLETA
-a.setAttribute('value', '1A')
-b.setAttribute('value', '1B')
-c.setAttribute('value', '1C')
+// ENDERECO DO ARQUIVO JSON
+const url = 'data.json'
 
-// PARA MONTAR AS PROXIMAS QUESTOES
-function proximaQuestao(nQuestao) {
-    numero.textContent = nQuestao
-    numQuestao.textContent = questoes[nQuestao].numQuestao
-    pergunta.textContent   = questoes[nQuestao].pergunta
-    a.textContent = questoes[nQuestao].alternativaA
-    b.textContent = questoes[nQuestao].alternativaB
-    c.textContent = questoes[nQuestao].alternativaC
-    a.setAttribute('value', nQuestao+'A')
-    b.setAttribute('value', nQuestao+'B')
-    c.setAttribute('value', nQuestao+'C')
+function pegarDados(i) {
+
+  fetch(url).then(response =>{
+      
+        return response.json();
+
+      }).then(data => {
+
+        if(data.erro) {
+          console.log("Erro ao acessar o JSON")
+          return
+        }
+        
+        // passar o quantidade de questoes para a variavel
+        let qtdQuestoes = (data.questoes.length)-1
+        // escrver a qtdQuestoes para total
+        total.textContent = parseInt(qtdQuestoes)
+        
+        // passe o valor de i no parametro
+        atribuirDados(data, i)
+
+      })
+      
+} // fim pegarDados
+
+function atribuirDados(data, i) {
+  if(i >= data.questoes.length) {
+    //console.log('Fim das questoes')
+    i = 1
+  }
+    nQuestao.textContent = data.questoes[i].numQuestao
+    pergunta.textContent = data.questoes[i].pergunta
+
+    a.textContent = data.questoes[i].alternativaA
+    b.textContent = data.questoes[i].alternativaB
+    c.textContent = data.questoes[i].alternativaC
+    d.textContent = data.questoes[i].alternativaD
+    
+    numero.textContent = data.questoes[i].numQuestao
+    
+    let certa = document.querySelector('#correct')
+    certa.value = data.questoes[i].correta
+    //console.log(resposta)
 }
 
-function bloquearAlternativas() {
-    a.classList.add('bloqueado')
-    b.classList.add('bloqueado')
-    c.classList.add('bloqueado')
-}
+// COMECAR O QUIZ
+let questaoAtual = 1
+pegarDados(1)
 
-function desbloquearAlternativas() {
-    a.classList.remove('bloqueado')
-    b.classList.remove('bloqueado')
-    c.classList.remove('bloqueado')
+function proximaQuestao(numQuestao) {
+  let proxima = parseInt(numQuestao)
+  pegarDados(proxima)
 }
 
 function verificarSeAcertou(nQuestao, resposta) {
 
-    let numeroDaQuestao = nQuestao.value
-    console.log("Questão " + numeroDaQuestao)
+  let numeroDaQuestao = nQuestao.value
+  //console.log("Questão " + numeroDaQuestao)
 
-    let respostaEscolhida = resposta.textContent
-    console.log("RespU " + respostaEscolhida)
+  let respostaEscolhida = resposta.textContent
+  //console.log("RespU " + respostaEscolhida)
 
-    let certa = questoes[numeroDaQuestao].correta
-    console.log("RespC " + certa)
+  // usar o fetch para pegar os dados
+  pegarDados(numeroDaQuestao)
 
-    if(respostaEscolhida == certa) {
-        console.log("Acertou")
-        //respostaEsta.textContent = "Correta 😊"
-        pontos += 10 // pontos = pontos + 10
+  let respostaCorrect = correct.value
+  //console.log(respostaCorrect)
+
+  if(respostaEscolhida == respostaCorrect) {
+      //console.log("Acertou")
+      somAcerto.play()      
+      pontos += 10 // pontos = pontos + 10
+  } else {
+      console.log("Errou!")
+      somErro.play()
+  }
+
+  quantidadeDeQuestoes = parseInt(total.textContent)
+  //console.log("Total " + quantidadeDeQuestoes)
+
+  proxima = parseInt(numero.textContent)+1
+  //console.log("Próxima " + proxima)
+
+  setTimeout(function() {
+    
+    if(proxima > quantidadeDeQuestoes) {
+        console.log('Fim do Jogo!')
+        fimDoJogo()
     } else {
-        console.log("Errou!")
-        //respostaEsta.textContent = "Errada 😢"
+        proximaQuestao(proxima)
     }
+  }, 50)
 
-    // atualizar placar
-    placar = pontos
-    instrucoes.textContent = "Pontos " + placar
+  // atualizar o placar
+  atualizarPlacar()
 
-    // bloquear a escolha de opcoes
-    bloquearAlternativas()
+}
 
-    setTimeout(function() {
-        //respostaEsta.textContent = '...'
-        proxima = numeroDaQuestao+1
-
-        if(proxima > totalDeQuestoes) {
-            console.log('Fim do Jogo!')
-            fimDoJogo()
-        } else {
-            proximaQuestao(proxima)
-        }
-    }, 250)
-    desbloquearAlternativas()
+function atualizarPlacar() {
+  placar.textContent = pontos
 }
 
 function fimDoJogo() {
-    instrucoes.textContent = "Fim de Jogo!"
-    numQuestao.textContent = ""
 
-    let pont = ''
-    pontos == 0 ? pont = 'ponto' : pont = 'pontos'
+  somAplausos.play()
 
-    pergunta.textContent   = "Você conseguiu " + pontos + " " + pont
+  let s = 's'
+  pontos == 0 ? s = '' : s = s
+  instrucoes.textContent = "Fim de Jogo! Você conseguiu " + pontos + " ponto"+ s
 
-    aviso.textContent = "Você conseguiu " + pontos + " " + pont
+  instrucoes.classList.add('placar')
 
-    a.textContent = ""
-    b.textContent = ""
-    c.textContent = ""
+  // OCULTAR O ARTICLE DA QUESTAO
+  questao.style.display = 'none'
 
-    a.setAttribute('value', '0')
-    b.setAttribute('value', '0')
-    c.setAttribute('value', '0')
+  setTimeout(function() {
+      pontos = 0 // zerar placar
+      instrucoes.classList.remove('placar')
 
-    // OCULTAR O ARTICLE DA QUESTAO
-    articleQuestoes.style.display = 'none'
+      // REINICIAR O JOGO
+      questao.style.display = 'block'
+      proximaQuestao(1)
+      instrucoes.textContent = 'Leia a questão e clique na resposta correta'
+  }, 7000)
 
-    setTimeout(function() {
-        pontos = 0 // zerar placar
-        location.reload();
-    }, 2000)
 }
